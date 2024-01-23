f05aadf863190cd1df5596304393d299c5bacb6d
Graperial <gengelmann@mail.bradley.edu>
2024-01-23 16:42:12 -0600

    cinemachine + single empty script
    lol

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private GameObject cheese;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cheese = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
